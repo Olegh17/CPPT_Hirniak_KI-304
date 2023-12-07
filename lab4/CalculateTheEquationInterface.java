356e11f8d0141df33d854d5b889124da109f4785
Olegh17 <57833447+Olegh17@users.noreply.github.com>
2023-12-07 19:35:02 +0200

    Add files via upload

---
+++ lab4/CalculateTheEquationInterface.java
@@ -0,0 +1,6 @@
+package lab4Hirniak304;
+
+public interface CalculateTheEquationInterface {
+    double doCalculation(double variable);
+    double doCalculationWithInputInside();
+}
---
+++ lab4/EquationsApp.java
@@ -0,0 +1,12 @@
+package lab4Hirniak304;
+
+public class EquationsApp {
+    public static void main(String[] args) {
+        CalculateTheEquationInterface calc = new CalculateTheEquation();
+        System.out.println("  y=ctg(x)  = " + calc.doCalculation(300));
+        System.out.println("  y=ctg(x)   = " + calc.doCalculation(70));
+        System.out.println("  y=ctg(x)   = " + calc.doCalculation(-4444));
+
+        System.out.println("  y=ctg(x)    = " + calc.doCalculationWithInputInside());
+    }
+}
